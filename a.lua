653349789b7a76bf4caae72bb21f194f40c2bb22
Mateusz Armatys <mateusz.armatys@gmail.com>
2015-01-09 13:39:51 +0100

    Another test case

--- a.lua
+++ a.lua
@@ -1,9 +1,12 @@
 
+
 local A = {}
 A["aname"] = "<class A proto>"
+A["child"] = {["subaname"] = "the subaname"}
 A["new"] = function (self)
   local t = {}
   t["aname"] = "<class A>"
+  t["child"] = {["subaname"] = "the subaname"}
   local s = setmetatable(t,{["__index"] = self})
   return s
 end
@@ -11,6 +14,15 @@ A["afoo"] = function (self)
   local s = self["aname"]
   print("Hello " .. s)
   return self
+end
+A["arun"] = function (self, name)
+  print("arun",name)
+end
+A["abar"] = function (self, obj)
+  self:arun(obj["subaname"])
+  local s = obj["subaname"]
+  self:arun(s)
+  self["arun"](self,obj["subaname"])
 end
 return A
 
